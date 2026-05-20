9fc4c2117515d71500635c2543b3538f7b7c5891
MrGrief1 <maksurazov1502@gmail.com>
2026-05-20 18:40:17 +0300

    Add Atlas learn app

---
+++ Atlas learn/Atlas learn/Atlas_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atlas_learnApp.swift
+//  Atlas learn
+//
+//  Created by Maks on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Atlas_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
